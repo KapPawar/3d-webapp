390690f48c9bf85d1c6526ba4877eb3080e71f47
KapPawar <pawar.kapil.shyam@gmail.com>
2024-03-21 19:12:57 -0500

    update padding

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -24,7 +24,7 @@ const Model = () => {
         </h1>
 
         <div className="flex flex-col items-center mt-5">
-          <div className="w-full pr-1 h-[75vh] md:h-[90vh] overflow-hidden relative">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
             <ModelView />
           </div>
         </div>
